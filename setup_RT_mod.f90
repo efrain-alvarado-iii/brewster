04f62e60e724bbda879e2db0457b94803619b834
Efrain Alvarado III <93851127+efrain-alvarado-iii@users.noreply.github.com>
2025-07-17 15:40:37 -0700

    Updates with the substellar main branch
    See commit aef3edf from main.
    
    -Did not include any edits for nested sampling.
    -Does include the make file for UH cluster
    -Does not include the brewster_template.py edit*
    
    *Just replace the logcoarsePress and logfinePress with
    logcoarsePress = np.linspace(-4.0, 2.4, knots)
    logfinePress = np.linspace(-4.0, 2.4, 64)

--- setup_RT_mod.f90
+++ setup_RT_mod.f90
@@ -102,14 +102,18 @@ contains
        ! add up the taus to get extinction
        do ilayer = 1, nlayers
 
-          tau_cloud(ilayer,:) = patch(ipatch)%atm(ilayer)%opd_ext
+          if (clphot) then
+             tau_cloud(ilayer,:) = patch(ipatch)%atm(ilayer)%opd_ext
+          end if
 
-          tau_others(ilayer,:) = patch(ipatch)%atm(ilayer)%opd_lines + &
+          if (othphot) then
+             tau_others(ilayer,:) = patch(ipatch)%atm(ilayer)%opd_lines + &
                patch(ipatch)%atm(ilayer)%opd_CIA + &
                patch(ipatch)%atm(ilayer)%opd_rayl + &
                patch(ipatch)%atm(ilayer)%opd_hmbff
-          
-          
+          end if
+
+
           patch(ipatch)%atm(ilayer)%opd_ext = &
                patch(ipatch)%atm(ilayer)%opd_ext + &
                patch(ipatch)%atm(ilayer)%opd_lines + &
@@ -164,9 +168,9 @@ contains
 
           ! get the pressure level where tau_cloud = taup_cl
           ! set reference tau for cloud taup_cl
-          taup_cl = 1.0
+          taup_cl = 0.6667
           ! set reference tau for others taup_oth
-          taup_oth = 1.0
+          taup_oth = 0.6667
 
 
           cldone = .false.
